942a52b72debd597af386a8765c3ff1fcef53797
Udayan Shevade <udayan.shevade@gmail.com>
2015-11-08 04:42:48 -0800

    Remove style calcs from sliding pizza generation

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -501,8 +501,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
